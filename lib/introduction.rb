4929f769561a9883ce56d0de912a49653c03888c
Jesse Brown <brownjesse8895@yahoo.com>
2020-12-04 01:44:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,12 +10,12 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language("Jesse", "Ruby")
+
 
 
   
-  def introduction_with_language_optional(name, laguage = "ruby")
-    puts "Hi, my name is #{name} and I am a learning to progaram in #{language}."
+def introduction_with_language_optional(name, language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
